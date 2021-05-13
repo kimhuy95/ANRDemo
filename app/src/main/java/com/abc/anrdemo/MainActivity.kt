aca6166790a4c93de0cf313e9fa5b0acb7bc06d4
Kim Huy <huy.phan@dirox.net>
2021-05-13 14:02:43 +0700

    Initial

---
+++ app/src/main/java/com/abc/anrdemo/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.abc.anrdemo
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.projection.MediaProjectionManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private val permissions = listOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    private val mediaProjectionManager by lazy { getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recordButton.setOnClickListener {
+            if (checkPermissions()) {
+                requestProjection()
+            } else {
+                ActivityCompat.requestPermissions(this, permissions.toTypedArray(), 100)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 101 && resultCode == Activity.RESULT_OK) {
+            startRecordingService(resultCode, data)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 100 && checkPermissions()) {
+            requestProjection()
+        }
+    }
+
+    private fun requestProjection() {
+        startActivityForResult(mediaProjectionManager.createScreenCaptureIntent(), 101)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun startRecordingService(resultCode: Int, resultData: Intent?) {
+        val intent = Intent(this, MainService::class.java).apply {
+            putExtra(EXTRA_RESULT_CODE, resultCode)
+            putExtra(EXTRA_RESULT_DATA, resultData)
+        }
+        startForegroundService(intent)
+    }
+
+    private fun checkPermissions(): Boolean {
+        return permissions.all { ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED }
+    }
+
+    companion object {
+        const val EXTRA_RESULT_CODE = "extra_result_code"
+        const val EXTRA_RESULT_DATA = "extra_result_data"
+    }
+}
---
+++ app/src/main/java/com/abc/anrdemo/MainService.kt
@@ -0,0 +1,206 @@
+package com.abc.anrdemo
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.Point
+import android.hardware.display.DisplayManager
+import android.media.MediaRecorder
+import android.media.projection.MediaProjection
+import android.media.projection.MediaProjectionManager
+import android.os.Build
+import android.os.Environment
+import android.os.IBinder
+import android.util.Log
+import android.util.Size
+import android.view.Display
+import android.view.WindowManager
+import androidx.core.app.NotificationCompat
+import java.io.File
+import java.util.concurrent.Executors
+
+class MainService: Service() {
+    private val mediaProjectionManager by lazy { getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager }
+
+    private var mediaRecorder: MediaRecorder? = null
+
+    private var projection: MediaProjection? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        val notification = configureNotification()
+        startForeground(222, notification)
+    }
+
+    private fun configureNotification(): Notification {
+        createNotificationChannel(this)
+        return NotificationCompat.Builder(applicationContext, "anr_channel_id")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .build()
+    }
+
+    private fun createNotificationChannel(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "anr_channel"
+            val descriptionText = "anr_channel_desc"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("anr_channel_id", name, importance).apply {
+                description = descriptionText
+                setSound(null, null)
+                enableLights(false)
+                lightColor = Color.BLUE
+                enableVibration(false)
+
+            }
+            val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Executors.newSingleThreadExecutor().submit {
+            intent?.let {
+                val resultCode = intent.getIntExtra(MainActivity.EXTRA_RESULT_CODE, Activity.RESULT_OK)
+                val resultData = intent.getParcelableExtra<Intent>(MainActivity.EXTRA_RESULT_DATA)
+                if (resultData != null) {
+                    projection = mediaProjectionManager.getMediaProjection(resultCode, resultData)
+                    val file = createVideoFile()
+                    val size = prepareMediaRecorder(file)
+                    configureMediaProjection(size)
+                    mediaRecorder?.start()
+                }
+            }
+        }
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    private fun configureMediaProjection(size: Size) {
+        val ratio = getFullScreenSize().width / size.width.toFloat()
+        Log.d("ggwp", "$size ${getFullScreenSize()} ${resources.displayMetrics.densityDpi} ${resources.displayMetrics.density} $ratio ${resources.displayMetrics.densityDpi / ratio}")
+        projection?.createVirtualDisplay(
+            getString(R.string.app_name),
+            size.width,
+            size.height,
+            resources.displayMetrics.densityDpi,
+            DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+            mediaRecorder?.surface,
+            null,
+            null
+        )
+    }
+
+    private fun prepareMediaRecorder(outputFile: File): Size {
+        val fullScreenSize = getFullScreenSize()
+        val screenWidth = fullScreenSize.width
+        val screenHeight = fullScreenSize.height
+        val screenRatio = screenWidth / screenHeight.toFloat()
+
+        val candidateWidths = listOf(1920, 1440, 1280, 720, 640, 320)
+        for (candidateWidth in candidateWidths) {
+            val candidateHeight = (candidateWidth / screenRatio).toInt()
+            val isValidCandidate = candidateWidth < screenWidth && candidateHeight < screenHeight
+            if (isValidCandidate) {
+                val width = ensureEvenSize(candidateWidth)
+                val height = ensureEvenSize(candidateHeight)
+                try {
+                    mediaRecorder = createMediaRecorder()
+                    mediaRecorder?.setOutputFile(outputFile.path)
+                    mediaRecorder?.setVideoSize(width, height)
+                    mediaRecorder?.prepare()
+                    return Size(width, height)
+                } catch (e: Exception) {
+                    Log.d("ggwp", e.message!!)
+                    try {
+                        mediaRecorder?.release()
+                    } catch (_: Exception) {
+                    }
+                    mediaRecorder = null
+                    // try next candidate width
+                }
+            }
+        }
+
+        throw IllegalStateException("Fullscreen recorder: can not prepare media recorder")
+
+    }
+
+    private fun createMediaRecorder(): MediaRecorder {
+        val canRecordAudio = false
+        val audioChannels = 1
+        val audioBitrate = 128000
+        val audioSampleRate = 44100
+
+        val videoFrameRate = 30
+        val videoBitrate = 1024 * 1024 * 4
+
+        return MediaRecorder().apply {
+            if (canRecordAudio) {
+                setAudioSource(MediaRecorder.AudioSource.MIC)
+            }
+            setVideoSource(MediaRecorder.VideoSource.SURFACE)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            if (canRecordAudio) {
+                setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+                setAudioChannels(audioChannels)
+                setAudioEncodingBitRate(audioBitrate)
+                setAudioSamplingRate(audioSampleRate)
+            }
+            setVideoEncoder(MediaRecorder.VideoEncoder.H264)
+            setVideoFrameRate(videoFrameRate)
+            setVideoEncodingBitRate(videoBitrate)
+        }
+    }
+
+    private fun getFullScreenSize(): Size {
+        /* Use DeviceManager to avoid android.os.strictmode.IncorrectContextUseViolation when StrictMode is enabled on API 30. */
+        val display =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                val displayManager =
+                    getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+                displayManager.getDisplay(Display.DEFAULT_DISPLAY)
+            } else {
+                (getSystemService(Context.WINDOW_SERVICE) as WindowManager)
+                    .defaultDisplay
+            }
+        val size = Point()
+        display.getRealSize(size)
+        val width = ensureEvenSize(size.x)
+        val height = ensureEvenSize(size.y)
+        return Size(width, height)
+    }
+
+    private fun ensureEvenSize(size: Int): Int {
+        return if (size % 2 == 0) {
+            size
+        } else {
+            size - 1
+        }
+    }
+
+    private fun createVideoFile() = File(getVideoDirectory(), "${generateVideoFilename()}.mp4")
+
+    private fun getVideoDirectory(): File {
+        val path =
+            File("${Environment.getExternalStorageDirectory().absolutePath}/${getString(R.string.app_name)}")
+        if (!path.exists()) {
+            path.mkdirs()
+        }
+        return path
+    }
+
+    private fun generateVideoFilename(): String {
+        val currentTime = System.currentTimeMillis()
+        return "video_$currentTime"
+    }
+}
